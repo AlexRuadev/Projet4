f558209478a673afd402622777cc918f1ba563f1
alexruadev <alexruadev@gmail.com>
2019-03-26 21:03:03 +0700

    header css try

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,14 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require('../css/header.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 require('popper.js');
 require('bootstrap/dist/css/bootstrap.css');
+require('bootstrap/dist/js/bootstrap.js');
+
 
 //console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
