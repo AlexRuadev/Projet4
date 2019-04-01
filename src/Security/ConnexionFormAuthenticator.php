92579b7d6e47b96c1388ea2539ae965fb79900f0
BaptisteBobot <bobot.baptiste@gmail.com>
2019-04-01 16:18:41 +0200

    Register over

--- src/Security/ConnexionFormAuthenticator.php
+++ src/Security/ConnexionFormAuthenticator.php
@@ -2,94 +2,53 @@
 
 namespace App\Security;
 
-use App\Entity\Parents;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ParentsRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class ConnexionFormAuthenticator extends AbstractFormLoginAuthenticator
 {
-    use TargetPathTrait;
+    private $parentsRepository;
+    private $router;
 
-    private $entityManager;
-    private $urlGenerator;
-    private $csrfTokenManager;
-
-    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager)
+    public function __construct(ParentsRepository $parentsRepository, RouterInterface $router)
     {
-        $this->entityManager = $entityManager;
-        $this->urlGenerator = $urlGenerator;
-        $this->csrfTokenManager = $csrfTokenManager;
+        $this->parentsRepository = $parentsRepository;
+        $this->router = $router;
     }
-
     public function supports(Request $request)
     {
-        return 'app_login' === $request->attributes->get('_route')
+        // do your work when we're POSTing to the login page
+        return $request->attributes->get('_route') === 'app_login'
             && $request->isMethod('POST');
     }
-
     public function getCredentials(Request $request)
     {
-        $credentials = [
+        return [
             'Parents_pseudo' => $request->request->get('Parents_pseudo'),
-            'password' => $request->request->get('password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
+            'Parents_mdp' => $request->request->get('Parents_mdp'),
         ];
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['Parents_pseudo']
-        );
-
-        return $credentials;
     }
-
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException();
-        }
-
-        $user = $this->entityManager->getRepository(Parents::class)->findOneBy(['Parents_pseudo' => $credentials['Parents_pseudo']]);
-
-        if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException(' Parents_pseudo could not be found.');
-        }
-
-        return $user;
+        return $this->parentsRepository->findOneBy(['Parents_pseudo' => $credentials['Parents_pseudo']]);
     }
-
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // Check the user's password or other credentials and return true or false
-        // If there are no credentials to check, you can just return true
-        throw new \Exception('TODO: check the credentials inside '.__FILE__);
+        // only needed if we need to check a password - we'll do that later!
+        return true;
     }
-
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
-        }
-
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->router->generate('home'));
     }
-
     protected function getLoginUrl()
     {
-        return $this->urlGenerator->generate('app_login');
+
     }
 }
