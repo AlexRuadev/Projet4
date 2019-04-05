160a0df9d0c59ce4d61a5ccff885f30dbe1b256d
Gauthier Guillaume <gauthier.guigz@gmail.com>
2019-04-05 16:57:39 +0200

    formulaire et js + test paiement

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -20,8 +20,17 @@ class PaymentController extends AbstractController
     /**
      * @Route("/stripe", name="stripe")
      */
-    public function payment()
+    public function payment(Request $request)
     {
+        \Stripe\Stripe::setApiKey("sk_test_gUMcR5A644I85YtgJj8Pr1P700iehIRShK");
+
+        \Stripe\Charge::create([
+        "amount" => 2000,
+        "currency" => "eur",
+        "source" => $request->request->get('stripeToken'), // obtained with Stripe.js
+        "description" => "paiement de test"
+        ]);
+
         return $this->render('payment/stripe.html.twig', [
             'controller_name' => 'PaymentController',
         ]);
