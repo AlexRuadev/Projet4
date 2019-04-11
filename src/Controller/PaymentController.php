7fdcae7f9ec8e5f810568bf4bd1c78f72075e58e
Gauthier Guillaume <gauthier.guigz@gmail.com>
2019-04-11 12:20:41 +0200

    ajout objet

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -25,11 +25,13 @@ class PaymentController extends AbstractController
         \Stripe\Stripe::setApiKey("sk_test_gUMcR5A644I85YtgJj8Pr1P700iehIRShK");
 
         \Stripe\Charge::create([
-          "amount" => 500,
-          "currency" => "eur",
+          "amount" => 500, // le montant en centimes
+          "currency" => "eur", // la monnaie
           "source" => "tok_amex", // obtained with Stripe.js
-          "description" => "TEST PAIEMENT",
-          "receipt_email" => "gauthier.guigz@gmail.com"
+          "description" => "TEST PAIEMENT", // description sur le paiement et contenu du mail recu a l'utilisateur
+          "customer"=> "cus_AJ6rlf2taOsyXj", // ID du client qui envoie le paiement
+          "on_behalf_of"=> null, // le compte stripe de l'entreprise qui recoit le paiement
+          "receipt_email" => "mailutilisateur@gmail.com" // recu du paiement au mail du client
           ]);
           
           return $this->render('payment/paiement.html.twig', [
