011b9c6086a55f196c33b7bef189eae56c8dfc20
Dorian Legros <d.ratha76@hotmail.com>
2019-04-23 13:53:43 +0200

    requete bdd pour une seule entreprise

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Entreprises;
 
 class ListingController extends AbstractController
 {
@@ -12,8 +13,21 @@ class ListingController extends AbstractController
      */
     public function index()
     {
+
+    	$id = 2;
+    	$entreprises =  $this->getDoctrine()
+		    ->getRepository(Entreprises::class)
+		    ->find($id);
+
+	    if (!$entreprises) {
+		    throw $this->createNotFoundException(
+			    'No product found for id '. $id
+		    );
+	    }
+
+
         return $this->render('listing/listing.html.twig', [
-            'controller_name' => 'ListingController',
+            'entreprises' => $entreprises,
         ]);
     }
 }
