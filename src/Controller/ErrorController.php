4f189209cd36cd07984b0d868dc1f691c03b5ad4
alexruadev <alexruadev@gmail.com>
2019-04-24 12:53:19 +0900

    navbar dynamique 1/2

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class ErrorController extends AbstractController
 {
     /**
-     * @Route("/404", name="error")
+     * @Route("/404", name="error404")
      */
-    public function index()
+    public function error404()
     {
         return $this->render('404.html.twig', [
+            'controller_name' => 'ErrorController',
+        ]);
+    }
+
+    /**
+     * @Route("/403", name="error403")
+     */
+    public function error403()
+    {
+        return $this->render('403.html.twig', [
             'controller_name' => 'ErrorController',
         ]);
     }
--- src/Controller/PageDeGardeController.php
+++ src/Controller/PageDeGardeController.php
@@ -19,4 +19,5 @@ class PageDeGardeController extends AbstractController
     }
 
 
+
 }
